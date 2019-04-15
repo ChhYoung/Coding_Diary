d9b202c1c311a93097cf9cd2febd1e1c8d140a10
yangchonghuan <984924255@qq.com>
2019-04-15 22:58:34 +0800

    大作业用神经网络

---
+++ LeetCode/118.pascals-triangle.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode id=118 lang=cpp
+ *
+ * [118] Pascal's Triangle
+ *
+ * https://leetcode.com/problems/pascals-triangle/description/
+ *
+ * algorithms
+ * Easy (45.31%)
+ * Total Accepted:    241.2K
+ * Total Submissions: 532.4K
+ * Testcase Example:  '5'
+ *
+ * Given a non-negative integer numRows, generate the first numRows of Pascal's
+ * triangle.
+ * 
+ * 
+ * In Pascal's triangle, each number is the sum of the two numbers directly
+ * above it.
+ * 
+ * Example:
+ * 
+ * 
+ * Input: 5
+ * Output:
+ * [
+ * ⁠    [1],
+ * ⁠   [1,1],
+ * ⁠  [1,2,1],
+ * ⁠ [1,3,3,1],
+ * ⁠[1,4,6,4,1]
+ * ]
+ * 
+ * 
+ */
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        
+    }
+};
+
