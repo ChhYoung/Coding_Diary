832dd2ea2b8aafc94cbfb19a689467d50c22854b
yangchonghuan <984924255@qq.com>
2019-06-14 19:46:50 +0800

    DSA

--- DSA/include/src/BinNode.h
+++ DSA/include/src/BinNode.h
@@ -1,106 +1,99 @@
+// ./DSA/include/src/BinNode.h
 #ifndef _DSA_INCLUDE_SRC_BINNODE_H__
 #define _DSA_INCLUDE_SRC_BINNODE_H__ 
 
 namespace DSA {
 
-// 红黑树节点颜色
 enum class RBColor {
 	RB_RED,
-	RB_BALCK,
+	RB_BLACK,
 };
 
-// 二叉树节点
+// 二叉树结点
 template<typename T>
 struct BinNode {
+// 成员
+	// 为方便定义一个指向节点的指针
 	typedef BinNode<T>* Ptr;
-// 数据
-	T data;
+	// 数值
+	T data_;
 	// 父节点
-	Ptr parent;
+	Ptr parent_;
 	// 左孩子
-	Ptr LChild;
+	Ptr lChild_;
 	// 右孩子
-	Ptr RChild;
+	Ptr rChild_;
 	// 高度
-	int height;
-	// 红黑树节点颜色
-	RBColor color;
+	int height_;
+	// 若为红黑树，则有颜色属性
+	RBColor color_;
 
 // 构造函数
 	BinNode()
-		: data()
-		, parent(nullptr)
-		, LChild(nullptr)
-		, RChild(nullptr)
-		, height(-1)
-		, color(RBColor::RB_RED)
+		: data_()
+		, parent_(nullptr)
+		, lChild_(nullptr)
+		, rChild_(nullptr)
+		, height_(-1)
+		, color_(RBColor::RB_RED)
 	{}
 
-	BinNode(const T& data,Ptr parent = nullptr,Ptr LChild=nullptr,Ptr RChild=nullptr,int height=0,RBColor color=RBColor::RB_RED)
-		: data(data)
-		, parent(parent)
-		, LChild(LChild)
-		, RChild(RChild)
-		, height(height)
-		, color(color)
+	BinNode(const T& data, Ptr parent = nullptr, Ptr lChild = nullptr, Ptr rChild = nullptr, int height = 0, RBColor color = RBColor::RB_RED)
+		: data_(data)
+		, parent_(parent)
+		, lChild_(lChild)
+		, rChild_(rChild)
+		, height_(height)
+		, color_(color)
 	{}
-
 // 操作接口
 	// 统计当前节点的后代总数
-	int size() const{
+	int size() const {
 		int s = 1;
-		if (LChild) { s += LChild->size(); }
-		if (RChild) { s += RChild->size(); }
+		if (lChild_) { s += lChild_->size(); }
+		if (rChild_) { s += rChild_->size(); }
 		return s;
 	}
 
-	bool operator<(const BinNode& rhs) const { return this->data < rhs.data; }
-	bool operator>(const BinNode& rhs) const { return this->data > rhs.data; }
-	bool operator==(const BinNode& rhs) const { return this->data == rhs.data; }
-	bool operator!=(const BinNode& rhs) const { return this->data != rhs.data; }
+	// 重载操作符
+	bool operator<(const BinNode& rhs) const { return this->data_ < rhs.data_; }
+	bool operator>(const BinNode& rhs) const { return this->data_ > rhs.data_; }
+	bool operator==(const BinNode& rhs) const { return this->data_ == rhs.data_; }
+	bool operator!=(const BinNode& rhs) const { return this->data_ != rhs.data_; }
 
-	// BinNode 状态性质判断
-	inline bool isRoot() const { return !parent };
-	inline bool isLChild() const { return parent && parent->LChild == this; }
-	inline bool isRChild() const { return parent && parent->RChild == this; }
-	inline bool hasParent() const { return parent; }
-	inline bool hasLChild() const { return LChild; }
-	inline bool hasRChild() const { return RChild; }
-	inline bool hasChild() const { return LChild || RChild; }
-	inline bool hasBothChild() const{ return LChild && RChild; }
-	inline bool isLeaf() const { return !LChild && !RChild; }
-	// 兄弟
-	inline Ptr sibling() const { 
-		return isLChild() ? parent->RChild : LChild;
-	}
-	inline Ptr uncle() const {
-		return parent->isLChild() ? parent->parent->RChild : parent->parent->LChild;
-	}
+	// 一些常用的接口
+	inline bool isRoot() const { return !parent_; }
+	inline bool isLChild() const { return parent_ && parent_->lChild_ == this; }
+	inline bool isRChild() const { return parent_ && parent_->rChild_ == this; }
+	inline bool hasParent() const { return parent_; }
+	inline bool hasLChild() const { return lChild_; }
+	inline bool hasRChild() const { return rChild_; }
+	inline bool hasChild() const { return lChild_ || rChild_; }
+	inline bool hasBothChild() const { return lChild_ && rChild_; }
+	inline bool isLeaf() const { return lChild_ && rChild_; }
+	// 得到兄弟节点
+	inline Ptr sibling() const { return isLChild() ? parent_->rChild_ : parent_->lChild_; }
+	// 得到叔叔节点
+	inline Ptr uncle() const { return parent_->isLChild() ? parent_->parent_->rChild_ : parent_->parent_->lChild_; }
+	// -----------------------------------------------------------------------------------------
+	// 插入
+	Ptr insertAsLeftChild(const T& data) { return lChild_ = new BinNode(data, this); }
+	Ptr insertAsRightChild(const T& data) { return rChild_ = new BinNode(data, this); }
 
-	//  插入节点
-	Ptr insertAsLeftChild(const T& data) {
-		// 返回插入的节点，之前的节点变为父节点
-		return (LChild = new BinNode(data, this));
-	}
-	Ptr insertAsRightChild(const T& data) {
-		// 返回插入的节点，之前的节点变为父节点
-		return (RChild = new BinNode(data, this));
-	}
+};// end of BinNode
 
-};
 
-/****************************     辅助函数   ************************************/
+ /**********************************************************************************************************/
+//  一些辅助函数
 
 // 节点高度
 template<typename T>
 int stature(BinNode<T>* node) {
-	return node ? node->height : -1;
+	return node ? node->height_ : -1;
 }
 
+
+
+
 }
-
-
-
-
-
 #endif //  _DSA_INCLUDE_SRC_BINNODE_H__
--- DSA/include/src/BinTree.h
+++ DSA/include/src/BinTree.h
@@ -1,20 +1,114 @@
+// ./DSA/include/src/BinTree.h
 #ifndef _DSA_INCLUDE_SRC_BINTREE_H__
-#define _DSA_INCLUDE_SRC_BINTREE_H__ 
+#define _DSA_INCLUDE_SRC_BINTREE_H__
+#include"BinNode.h" 
 #include"release.h"
-#include"BinNode.h"
-
-#if TEST_BUILD
-#include<stdlib.h> // rand
-#endif 
 
 namespace DSA {
 
+	// 二叉树
+template<typename T>
+class BinTree {
+public:
+	typedef BinNode<T> Node;
+	typedef Node* NodePtr;
+	typedef BinTree<T> BinTreeType;
+	typedef BinTreeType* BinTreePtr;
+
+protected:
+		// 规模
+	int size_;
+		// 树根
+	NodePtr root_;
+
+		// 更新节点node的高度
+		// 高度为子节点最大高度加一，每当节点有所增减时其高度都需要更新，但是实际过程中节点很难发现子节点的变化，所以
+		//		采取当有节点加入或者离开树时 更新其祖先的高度
+		// 实现： v节点只需取左右孩子的最大高度 + 1 即可,v出发沿parent指针逆行向上,依次更新高度
+		// 优化版本: 
+		// 1. 若在1更新的过程中发现某一祖先的高度没有更新则可停止， 即若改变节点C，若A的最深路径不经过C，
+		//     则A的高度自然不会会改变
+		// 2. 改进后复杂度不变，但减少了不必要的更新
+
+	// 原始版本
+	virtual int updateHeight(NodePtr node) {
+		auto lh = stature(node);
+		auto rh = stature(node);
+		return node->height_ = 1 + (lh < rh ? rh : lh);
+	}
+	// 优化版本
+	virtual int  updateHeightAbove(NodePtr node) {
+		while (node) {
+			int old_height = node->height_;
+			if (old_height == updateHeight(node)) {
+				break;
+			}
+			node = node->parent_;
+		}
+	}
+
+public:
+	// 来自父亲的引用
+	NodePtr& fromParentTo(NodePtr node) {
+		return node->isRoot() ? root_ : (node->isLChild() ? node->parent_->lChild_ : node->parent_->rChild_);
+	}
+
+	// 三种插入节点的方式 ,在原先无节点的情况下
+	// 作为根节点，左子节点，右子节点
+	NodePtr insertAsRootNode(T const& data) {
+		size_ = 1;
+		return (root_ = new Node(data));
+	}
+
+	NodePtr insertAsLeftChild(NodePtr node,T const& data) {
+		size_++;
+		node->insertAsLeftChild(data);
+		updateHeightAbove(node);
+		return node->lChild_;
+	}
+
+	NodePtr insertAsRightChild(NodePtr node, T const& data) {
+		size_++;
+		node->insertAsRightChild(data);
+		updateHeightAbove(node);
+		return node->rChild_;
+	}
+	// 子树接入
+	// lc作为节点x的左子树接入，lc本身置空
+	NodePtr attachAsLChild(NodePtr x, BinTreePtr& lc) {
+		if (x->lChild_ = lc->root_) {
+			x->lChild_->parent_ = x;
+		}
+		size_ += lc->size_;
+		updateHeightAbove(x);
+		lc->root_ = nullptr;
+		lc->size_ = 0;
+		release(lc);
+		lc = nullptr;
+		return x;
+	}
+
+	// rc作为节点x的右子树接入，rc本身置空
+	NodePtr attachAsRChild(NodePtr x, BinTreePtr& rc) {
+		if (x->rChild_ = rc->root_) {
+			x->rChild_->parent_ = x;
+		}
+		size_ += rc->size_;
+		updateHeightAbove(x);
+		rc->root_ = nullptr;
+		rc->size_ = 0;
+		release(rc);
+		rc = nullptr;
+		return x;
+	}
 
 
 
 
 
 
+};// end of BinTree
 
-}
-#endif // _DSA_INCLUDE_SRC_BINTREE_H__ 
+} // end of namespace
+
+#endif // _DSA_INCLUDE_SRC_BINTREE_H__
