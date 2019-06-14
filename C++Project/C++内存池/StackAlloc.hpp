274a1d3c5b2063561b558f2337687749530478c3
yangchonghuan <984924255@qq.com>
2019-06-15 00:59:29 +0800

    C++ Mem Pool

---
+++ C++Project/C++内存池/StackAlloc.hpp
@@ -0,0 +1,70 @@
+#ifndef STACK_ALLOC_H__
+#define STACK_ALLOC_H__
+
+#include<memory> 
+
+template<typename T>
+// 用链式结构来实现栈
+struct StackNode_{
+    // 数据
+    T data;
+    // 前一个节点
+    StackNode_* prev;
+};
+
+template <typename T, typename Alloc = std::allocator<T>>
+class StackAlloc{
+public: 
+    typedef StackNode_<T> Node;
+    // 得到 std::allocator<StackNode_<T>>
+    typedef typename Alloc::template rebind<Node>::other allocator;
+
+    StackAlloc(){head_ = 0;}
+    ~StackAlloc(){ clear();}
+
+    // 当栈中元素为空时返回 true
+    bool empty(){return (head_ == 0);}
+    // 释放栈中元素的所有内存
+    void clear(){
+        Node* curr = head_;
+        // 依次出栈
+        while(curr != 0){
+            Node* tmp = curr->prev;
+            // 先析构再回收内存
+            allocator_.destroy(curr);
+            allocator_.deallocate(curr,1);
+            curr = tmp;
+        }
+        head_ = 0;
+    }
+    // 压栈
+    void push(T element){
+        // 为一个节点分配内存
+        Node* newNode = allocator_.allocate(1);
+        // 调节节点构造函数
+        allocator_.construct(newNode,Node());
+        // 入栈
+        newNode->data = element;
+        newNode->prev = head_;
+        head_ = newNode;
+    }
+    // 出栈
+    T pop(){
+        T result = head_->data;
+        Node* tmp = head_->prev;
+        allocator_.destroy(head_);
+        allocator_.deallocate(head_,1);
+        head_ = tmp;
+        return result;
+    }
+    // 返回栈顶元素
+    T top(){
+        return head_->data;
+    }
+private:
+    allocator allocator_;
+    // 栈顶
+    Node* head_;
+};
+
+#endif // STACK_ALLOC_H__
--- C++Project/C++内存池/main.cpp
+++ C++Project/C++内存池/main.cpp
@@ -12,10 +12,37 @@
 // 重复次数
 #define REPS 100
 
-int main{
+int main(){
 
     clock_t start;
     // 使用默认分配器 std::allocator
-    
+    StackAlloc<int,std::allocator<int>> stackDefalut;
+    start = clock();
+    for(int j = 0;j < REPS;++j){
+        assert(stackDefalut.empty());
+        for(int i = 0; i < ELEMS; ++i){
+            stackDefalut.push(i);
+        }
+        for(int i = 0; i < ELEMS; ++i){
+            stackDefalut.pop();
+        }
+    }
+    std::cout<<"Default Allocator Time: ";
+    std::cout<<(((double)clock() - start) / CLOCKS_PER_SEC)<<"\n\n";
+/* 
+    // 使用内存池
+    StackAlloc<int, MemoryPool<int>> stackPool;
+    start = clock();
+    for (int j = 0; j < REPS; j++) {
+        assert(stackPool.empty());
+        for (int i = 0; i < ELEMS; i++)
+          stackPool.push(i);
+        for (int i = 0; i < ELEMS; i++)
+          stackPool.pop();
+    }
+    std::cout << "MemoryPool Allocator Time: ";
+    std::cout << (((double)clock() - start) / CLOCKS_PER_SEC) << "\n\n";
+
+    */
     return 1;
 }
